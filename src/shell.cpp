34e047fe68ea011a2702adbd933d00c3458974c2
MaanooAk <akritasak@gmail.com>
2016-12-12 22:44:59 +0200

    Comments and style fixes

--- src/shell.cpp
+++ src/shell.cpp
@@ -14,12 +14,12 @@
 Shell::Shell() {
 	this->path = getcwd(NULL, PATH_MAX+1); // get path
 	this->child_pid = 0; // no child
-	
+
 	// Handle SIGCHLD by calling cleanZombies.
 	struct sigaction sigchld_action;
 	memset(&sigchld_action, 0, sizeof(sigchld_action));
 	sigchld_action.sa_handler = &cleanZombie;
-	sigaction (SIGCHLD, &sigchld_action, NULL);
+	sigaction(SIGCHLD, &sigchld_action, NULL);
 }
 
 void Shell::cleanZombie(int signal_number) {
@@ -28,16 +28,16 @@ void Shell::cleanZombie(int signal_number) {
 }
 
 bool Shell::setPath(const char* path) {
-	
+
 	// change path
 	if (chdir(path)) {
 		// failed
 		return false;
 	}
-	
+
 	// get and store resolved path
 	getcwd(this->path, PATH_MAX+1);
-	
+
 	return true;
 }
 
@@ -45,8 +45,8 @@ const char* Shell::getPath() const {
 	return this->path;
 }
 
-bool Shell::startSync(const char* program, char* const arguments[]) {	
-	
+bool Shell::startSync(const char* program, char* const arguments[]) {
+
 	return start(program, arguments, false);
 }
 
@@ -56,51 +56,51 @@ bool Shell::startAsync(const char* program, char* const arguments[]) {
 }
 
 bool Shell::start(const char* program, char* const arguments[], const bool async) {
-	
+
 	// TODO MAYBE convert arguments
-	
+
 	// fork
 	pid_t child_pid = fork();
-	
+
 	if (child_pid == 0) {
 		// child process
-		
+
 		// execvp: v = null terminated arguments, p = search in path
 		execvp(program, arguments);
-		
+
 		// Flow comes here only on error
 		std::cout << MES_COMMAND_NOT_FOUND_1 << program << MES_COMMAND_NOT_FOUND_2 << std::endl;
 		_exit(0);
-		
+
 	}
 	else {
 		// parrent
-		
+
 		if (!async) {
 			this->child_pid = child_pid;
-			
+
 			int status;
 			waitpid(child_pid, &status, 0);
-			
+
 			this->child_pid = 0; // child no longer alive
-			
+
 			if (!WIFEXITED(status)) {
 				return false;
 			}
 		}
 	}
-	
+
 	return true;
 }
 
 bool Shell::stopLast() {
-		
+
 	if(this->child_pid == 0) {
 		// no child running
 		return false;
 	}
-	
+
 	kill(this->child_pid, SIGTERM);
-	
+
 	return true;
 }
--- src/shell.h
+++ src/shell.h
@@ -2,17 +2,49 @@
 
 #include <sys/types.h>
 
+
+/**
+ * Starts synchronized and asynchronized processes and manipulates the working directory.
+ * Keeps track of the last started process.
+ *
+ * Only one instance of the Shell should be created.
+ *
+ * Used by the Handler.
+ */
 class Shell {
 public:
 
+	/**
+	 * Constructs and initializes the shell and registers a SIGCHLD signal handler.
+	 */
 	Shell();
 
+	/**
+	 * Set the working directory path.
+	 *
+	 * The '.' and '..' wildcards are accepted.
+	 */
 	bool setPath(const char* path);
+
+	/**
+	 * Get the current working directory path.
+	 */
 	const char* getPath() const;
 
+	/**
+	 * Start a synchronized process.
+	 *
+	 * Arguments is a null terminated char* array including program.
+	 */
 	bool startSync(const char* program, char* const arguments[]);
+
+	/**
+	 * Start an asynchronized process.
+	 *
+	 * Arguments is a null terminated char* array including program.
+	 */
 	bool startAsync(const char* program, char* const arguments[]);
-	
+
 	/**
 	 * Terminates the last program started with startSync()
 	 */
@@ -23,9 +55,10 @@ protected:
 	char* path;
 	pid_t child_pid;
 
-private:
-
 	bool start(const char* program, char* const arguments[], const bool async);
 
+private:
+
 	static void cleanZombie(int signal_number);
+
 };
