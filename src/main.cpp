a229b55d15ab0fae1bf47295fde5b0aaa8594920
MaanooAk <akritasak@gmail.com>
2016-12-12 22:46:15 +0200

    Added temp testShell in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,36 @@
 
 #include "defs.h"
 
+
+// Temp test functions
+// TODO move to test framework
+void testShell();
+
+
 int main(int argc, char *argv[]) {
 
 	std::cout << APP_NAME << " v" << APP_VERSION << std::endl;
 
+	//testShell();
+}
+
+
+#include "shell.h"
+void testShell() {
+	
+	Shell *s = new Shell();
+
+	std::cout << s->getPath() << std::endl;
+	std::cout << s->setPath("../..") << std::endl;
+	std::cout << s->getPath() << std::endl;
+
+	char s1[] = "ls";
+	char s2[] = "-l";
+	char* const args[] = {s1, s2, 0};
+
+	std::cout << s->startSync(s1, args) << std::endl;
+	std::cout << s->startSync(s1, args) << std::endl;
+
+	std::cin >> s1;
+	
 }
