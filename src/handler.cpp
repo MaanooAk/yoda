bfc69d3b7990c8fd8ffb1e2822d6452a62d5e675
jimboelessar <jimbo_elesar@yahoo.gr>
2017-01-19 23:38:12 +0200

    updated header and cpp files (#1)
    * Update handler.h
    
    * Update handler.cpp
    
    header file is technically fine, cpp file is incomplete.

--- src/handler.cpp
+++ src/handler.cpp
@@ -1,2 +1,64 @@
 #include "handler.h"
+#include "defs.h"
 
+
+Handler(Shell sh) {
+	this.sh = sh;
+}
+
+
+bool execute(Command c) {
+	const char* comm = new const char*;
+	const char* args[] = new const char*[];
+	comm = c.getCommand();
+	args = c.getArgs();
+	
+	if (strcmp(c.getCommand(), "cd") == 0) {
+		cd(c.getLast());
+		return true;
+	}
+	if (strcmp(c.getCommand(), "pwd") == 0) {
+		pwd();
+		return true;
+	}
+	if (strcmp(c.getCommand(), "exit") == 0) {
+		exitYoda();
+		return true;
+	}
+	if (strcmp(c.getCommand(), "help") == 0) {
+		//help();
+		return true;
+	}
+	
+	if (strcmp(c.getLast(), "&") == 0) {
+		//get to the last argument (the '&') to remove it
+		for(int i = 0; strcmp(args[i], "\0") != 0; i++)
+   		;
+  		args[--i] = '\0';
+		
+		sh.startAsync(comm, args);
+		return true;
+	}
+	else {
+		sh.startSync(comm, args);
+		return true;
+	}
+}
+
+bool cd(const char* path) {
+	return sh.setPath(path);
+}
+
+void pwd() {
+	const char* path = sh.getPath();
+	cout<<endl;
+	cout<<path<<endl;
+}
+
+void exitYoda() {
+	exit();
+}
+
+void help() {
+	
+}
--- src/handler.h
+++ src/handler.h
@@ -2,9 +2,41 @@
 
 class Handler {
 public:
+	
+	/**
+ 	 * Constructor
+ 	 */
+	Handler(Shell sh);
+	
+	/**
+	 * Executes a command.
+ 	 */
+	bool execute(const Command &c);
 
 protected:
 
 private:
+	
+	Shell sh;
+	
+	/*
+	 * Changes the active directory.
+	 */
+	bool cd(const char* path);
+	
+	/*
+	 * Displays the active directory.
+	 */
+	void pwd();
+	
+	/*
+	 * Closes the shell.
+	 */
+	void exitYoda();
+	
+	/*
+	 * Displays help list of commands.
+	 */
+	void help();
 
 };
