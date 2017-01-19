88bc13afcd2eb4045d7774f5c95b202bad0600f4
MaanooAk <akritasak@gmail.com>
2017-01-19 23:37:04 +0200

    Tests extensions and more tests

--- src/test.cpp
+++ src/test.cpp
@@ -11,45 +11,70 @@
 const char *cwd = getcwd(NULL, PATH_MAX+1);
 
 bool Test::perform() {
-	
-	std::cout << " Perform test: " << this->title << std::endl;
-	
+
+	std::cout << " " << this->title << RESET << ": " << this->subtitle << std::endl;
+
 	bool result = assert(execute());
-	
+
 	std::cout << "  " << (result ? MES_TEST_TRUE : this->must ? MES_TEST_FALSE : MES_TEST_SEMI) << std::endl;
+
 	return result;
 }
 
 Tests::Tests() {
-	tests.push_back(new TestShell01());	
-	tests.push_back(new TestShell02());	
-	tests.push_back(new TestShell03());	
+	this->tests.push_back(new TestShell01());
+	this->tests.push_back(new TestShell02());
+	this->tests.push_back(new TestShell03());
+	this->tests.push_back(new TestCommand01());
+	this->tests.push_back(new TestCommand02());
+	this->tests.push_back(new TestCommand03());
 }
 
 bool Tests::perform() {
 	bool result = true;
-	
-	for(auto i : tests) {
+
+	std::cout << "Perform tests: " << std::endl;
+	for (auto i : this->tests) {
 		result &= i->perform() || !i->must;
 	}
-	
-	return true;
+
+	return result;
 }
+
+// === Sub functions ===
+
+static bool compare(const char* s1, const char* s2) {
+	unsigned int i1 = 0, i2 = 0;
+	while (s1[i1] != '\0' && s1[i1] == s2[i2]) {
+		i1++;
+		i2++;
+	}
+	return s1[i1] == s2[i2];
+}
+
+static bool compare(const char* const* s1, const char* const* s2) {
+	unsigned int i1 = 0, i2 = 0;
+	while (s1[i1] != nullptr && compare(s1[i1], s2[i2])) {
+		i1++;
+		i2++;
+	}
+	return s1[i1] == s2[i2];
+}
+
+// === Tests ===
 
 const void* TestShell01::execute() {
 	Shell s;
 	return s.getPath();
 }
 
-// === Tests ===
-
 bool TestShell01::assert(const void* result) {
 	char path[PATH_MAX+1];
 	strcpy(path, cwd);
 	strcat(path, "");
-	
+
 	bool a = !strcmp((const char*)result, path);
-	
+
 	if (!a) {
 		std::cout << "  Found " << (const char*)result << " instead of " << path << std::endl;
 	}
@@ -68,9 +93,9 @@ bool TestShell02::assert(const void* result) {
 	char path[PATH_MAX+1];
 	strcpy(path, cwd);
 	strcat(path, "/src");
-	
+
 	bool a = !strcmp((const char*)result, path);
-	
+
 	if (!a) {
 		std::cout << "  Found " << (const char*)result << " instead of " << path << std::endl;
 	}
@@ -82,18 +107,63 @@ const void* TestShell03::execute() {
 	s.setPath(cwd);
 	char s1[] = "echo";
 	char s2[] = "  Echo command text";
-	char* const args[] = {s1, s2, 0};
+	char* const args[] = {s1, s2, nullptr};
 	bool flag = s.startSync(s1, args);
 	return new bool(flag);
 }
 
 bool TestShell03::assert(const void* result) {
-	
-	bool a = *((bool*)result);//!strcmp((const char*)result, path);
-	
+
+	bool a = *((bool*)result);
+
 	if (!a) {
 		std::cout << "  Found " << a << " instead of " << true << std::endl;
 	}
+	return a;
+}
+
+const void* TestCommand01::execute() {
+	char line[] = "ls build -a";
+	return new char(line[0]); // TODO change
+}
+
+bool TestCommand01::assert(const void* result) {
+	char s1[] = "ls";
+	char s2[] = "build";
+	char s3[] = "-a";
+	const char* const args[] = {s1, s2, s3, nullptr};
+
+	bool a = !compare(args, args);
+	return a;
+}
+
+const void* TestCommand02::execute() {
+	char line[] = "ls  build    -a";
+	return new char(line[0]); // TODO change
+}
+
+bool TestCommand02::assert(const void* result) {
+	char s1[] = "ls";
+	char s2[] = "build";
+	char s3[] = "-a";
+	const char* const args[] = {s1, s2, s3, nullptr};
+
+	bool a = !compare(args, args);
+	return a;
+}
+
+const void* TestCommand03::execute() {
+	char line[] = "ls \"build all\" -a";
+	return new char(line[0]); // TODO change
+}
+
+bool TestCommand03::assert(const void* result) {
+	char s1[] = "ls";
+	char s2[] = "\"build all\""; // TODO remove quotes?
+	char s3[] = "-a";
+	const char* const args[] = {s1, s2, s3, nullptr};
+
+	bool a = !compare(args, args);
 	return a;
 }
 
--- src/test.h
+++ src/test.h
@@ -5,28 +5,32 @@
 class Test {
 public:
 
-	Test(const bool must, const char* title) :
+	Test(const bool must, const char* title, const char* subtitle) :
 		must(must),
-		title(title) {};
+		title(title),
+		subtitle(subtitle) {};
+
+	virtual ~Test() {};
 
 	bool perform();
-	
+
 	virtual const void* execute() = 0;
-	
+
 	virtual bool assert(const void* result) = 0;
-	
+
 	const bool must;
-	
+
 	const char* title;
+	const char* subtitle;
 };
 
 class Tests {
 public:
-	
+
 	Tests();
-	
+
 	bool perform();
-	
+
 	std::vector<Test*> tests;
 };
 
@@ -35,8 +39,8 @@ public:
 class TestShell01 : public Test {
 public:
 	TestShell01() :
-		Test(true, "Get current working path") {}
-	
+		Test(true, "Shell", "Get current working path") {}
+
 	virtual const void* execute() override;
 	virtual bool assert(const void* result) override;
 };
@@ -44,8 +48,8 @@ public:
 class TestShell02 : public Test {
 public:
 	TestShell02() :
-		Test(true, "Change current working path") {}
-	
+		Test(true, "Shell", "Change current working path") {}
+
 	virtual const void* execute() override;
 	virtual bool assert(const void* result) override;
 };
@@ -53,9 +57,35 @@ public:
 class TestShell03 : public Test {
 public:
 	TestShell03() :
-		Test(true, "Run echo command with arguments") {}
-	
+		Test(true, "Shell", "Run echo command with arguments") {}
+
 	virtual const void* execute() override;
 	virtual bool assert(const void* result) override;
 };
 
+class TestCommand01 : public Test {
+public:
+	TestCommand01() :
+		Test(true, "Command", "Parse simple arguments") {}
+
+	virtual const void* execute() override;
+	virtual bool assert(const void* result) override;
+};
+
+class TestCommand02 : public Test {
+public:
+	TestCommand02() :
+		Test(false, "Command", "Parse arguments with multiple whitespace") {}
+
+	virtual const void* execute() override;
+	virtual bool assert(const void* result) override;
+};
+
+class TestCommand03 : public Test {
+public:
+	TestCommand03() :
+		Test(false, "Command", "Parse arguments with double quotes") {}
+
+	virtual const void* execute() override;
+	virtual bool assert(const void* result) override;
+};
