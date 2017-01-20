7e4497edbbf3b900a521dbd4f0ebe3edf2e8ffe9
asarlidou <anactaciaxd@gmail.com>
2017-01-20 17:50:30 +0200

    Command comments and fixes

--- src/command.cpp
+++ src/command.cpp
@@ -4,50 +4,58 @@
 
 
 Command::Command(const char* text) {
-
-	char buff[256];	
+	// buffer variables
+	char buff[256];
 	char* array[256];
-	
+	// indexes
 	int i = 0, ibuff = 0, iarray = 0;
-	
+
 	while(true) {
-		
+
 		if (text[i] != ' ' && text[i] != '\0') {
-			
+			// copy any normal char to buffer
+
 			buff[ibuff] = text[i];
 			ibuff += 1;
-			
+
 		}else if(ibuff > 0) {
-			
+			// do the split
+
 			buff[ibuff] = '\0';
 			ibuff += 1;
-			
+
 			char* arg = new char[ibuff];
 			String::copy(arg, buff);
-			
+
 			array[iarray] = arg;
 			iarray += 1;
-			
+
 			ibuff = 0;
 		}
-		
+
+		// exit on end of text
 		if(text[i] == '\0') {
 			break;
 		}
-		
+
 		i += 1;
 	}
-	
+
 	array[i] = nullptr;
-	
+
+	// set member variables
 	argc = i;
-	args = new char*[i+1];
-	String::copy(args, array);
+	argv = new char*[i+1];
+	String::copy(argv, array);
 
 }
 
 virtual Command::~Command() {
-	// TODO(asarlidou) implement
+	// delete the args
+	for (int i = 0; i < argc; i++) {
+		delete[] argv[i];
+	}
+	delete[] argv;
 }
 
 char* const* Command::getArgs() const {
