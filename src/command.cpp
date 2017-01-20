98fd9fb0dc4c205beb7d1340ecf7df4b90e8a9da
MaanooAk <akritasak@gmail.com>
2017-01-20 02:03:58 +0200

    Added empty command impls

--- src/command.cpp
+++ src/command.cpp
@@ -1,2 +1,21 @@
 #include "command.h"
 
+
+Command::Command(const char* text) {
+	// TODO(asarlidou) implement
+}
+
+char* const* Command::getArgs() const {
+	// TODO(asarlidou) implement
+	return nullptr;
+}
+
+const char* Command::getCommand() const {
+	// TODO(asarlidou) implement
+	return nullptr;
+}
+
+const char* Command::getLast() const {
+	// TODO(asarlidou) implement
+	return nullptr;
+}
--- src/command.h
+++ src/command.h
@@ -1,10 +1,17 @@
 #pragma once
 
+/**
+ * TODO(asarlidou) implement
+ */
 class Command {
 public:
 
+	Command(const char* text);
+
 	char* const* getArgs() const;
+
 	const char* getCommand() const;
+
 	const char* getLast() const;
 
 protected:
